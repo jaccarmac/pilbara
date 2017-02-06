b42dc33d79770202c1903b6e6f88e6f380796b45
jaccarmac <jaccarmac@gmail.com>
2017-02-06 11:09:02 +0000

    Simple arg parsing.
    FossilOrigin-Name: 19c533fa266d0b3b542e691e973efbe337092c08

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate argparse;
+
+use argparse::{ArgumentParser, Store};
+
 fn main() {
+    let mut host = "".to_string();
+    let mut port = 0;
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Configurable MUD client in TCOD.");
+        ap.refer(&mut host)
+            .add_argument("host",
+                          Store,
+                          "hostname of MUD server: discworld.starturtle.net")
+            .required();
+        ap.refer(&mut port)
+            .add_argument("port", Store, "port to connect to: 4242")
+            .required();
+        ap.parse_args_or_exit();
+    }
     println!("Hello, world!");
 }

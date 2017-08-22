ff08dfed132d94b23cfadbf45b4273b5602697a3
jaccarmac <jaccarmac@gmail.com>
2017-08-22 04:47:40 +0000

    Add clap-based arg parsing.
    FossilOrigin-Name: 14da99fb1be1d9879faea03ba69315976c6f4eb6

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,53 @@
 //! A configurable MUD client using TCOD for rendering. Designed around support
 //! for Discworld MUD, though that won't be the only target by any means.
+#![feature(conservative_impl_trait)]
+#![feature(use_extern_macros)]
 
-extern crate argparse;
-
-use argparse::{ArgumentParser, Store};
+#[macro_use]
+extern crate clap;
 
 fn main() {
-    let mut host = "".to_string();
-    let mut port: u16 = 0;
-    {
-        let mut ap = ArgumentParser::new();
-        ap.set_description("Configurable MUD client in TCOD.");
-        ap.refer(&mut host)
-            .add_argument("host",
-                          Store,
-                          "hostname of MUD server: discworld.starturtle.net")
-            .required();
-        ap.refer(&mut port)
-            .add_argument("port", Store, "port to connect to: 4242")
-            .required();
-        ap.parse_args_or_exit();
+    let matches = clap::clap_app!((clap::crate_name!()) =>
+        (version: clap::crate_version!())
+        (author: clap::crate_authors!())
+        (@arg host: * "MUD server to connect to:\tdiscworld.starturtle.net")
+        (@arg port:
+            *
+            {can_convert_to_valid("port number", in_port_number_range)}
+            "port on the server to connect to:\t4242")
+        ).get_matches();
+    println!(
+        "Connecting to {}:{}",
+        matches.value_of("host").unwrap(),
+        matches.value_of("port").unwrap()
+    );
+}
+
+fn can_convert_to_valid<
+    D: std::fmt::Display,
+    V: Fn(VT) -> Result<VO, VE>,
+    VT: std::str::FromStr,
+    VO,
+    VE: std::fmt::Display,
+>(
+    typedesc: D,
+    validator: V,
+) -> impl Fn(String) -> Result<(), String> {
+    move |s| match match s.parse::<VT>() {
+        Ok(value) => match validator(value) {
+            Ok(_) => Ok(()),
+            Err(reason) => Err(reason.to_string()),
+        },
+        Err(_) => Err(format!("\"{}\" is not valid", s)),
+    } {
+        ok @ Ok(_) => ok,
+        Err(reason) => Err(format!("bad {}: {}", typedesc, reason)),
     }
-    println!("Hello, world!");
+}
+
+fn in_port_number_range(port_number: u32) -> Result<(), &'static str> {
+    match port_number <= 65536 {
+        true => Ok(()),
+        false => Err("out of range (0 - 65536)"),
+    }
 }
